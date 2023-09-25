1d371a4032093bc17b990570f1ce65b4a67282d8
gasangw <thomasgasangwa@gmail.com>
2023-09-26 00:08:22 +0200

    Set up dependencies and starter files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="bg-yellow-700">
+      <h1 className='text-center text-red-700 text-3xl'>Hello Thomas</h1>
+    </div>
+  )
+}
+
+export default App
