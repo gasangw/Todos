e577460ebe161dd5e58e7ffddd82fdea59ef9768
gasangw <thomasgasangwa@gmail.com>
2023-09-26 11:18:17 +0200

    Style the input section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import TodoHeading from "./Components/Heading"
+import TextInput from "./Components/TextInput"
 function App() {
   return (
     <div>
       <TodoHeading />
+      <TextInput />
     </div>
   )
 }
--- src/Components/TextInput.jsx
+++ src/Components/TextInput.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function TextInput() {
   return (
-    <div>
-        <input type='text' />
+    <div className='flex justify-between rounded-md shadow-md w-3/5 mx-auto mt-8 px-6 items-center'>
+        <input type='text' placeholder='Add todo...' className='py-4 px-3 w-4/5 outline-none placeholder-black'/>
+        <p className='bg-green-600 py-2 px-2 rounded-full cursor-pointer'>
+          <FontAwesomeIcon icon={faPlus}  className="text-[#f5f5f5]"/>
+        </p>
     </div>
   )
 }
