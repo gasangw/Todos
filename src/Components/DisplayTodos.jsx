b6a553689232a7cd1dfcdf6fe71de6c46fab6b18
gasangw <thomasgasangwa@gmail.com>
2023-09-27 16:15:08 +0200

    Change color of text on checking the checkbox

--- src/Components/DisplayTodos.jsx
+++ src/Components/DisplayTodos.jsx
@@ -18,7 +18,7 @@ const handleChecked =()=>{
     <div className="flex justify-between items-center border-b-2 pb-2">
       <div className="flex gap-4">
         <input type="checkbox" onChange={handleChecked}/>
-        <p className="text-balck text-lg" style={{textDecoration: isChecked ? 'line-through' : '', fontStyle: isChecked ? 'italic': '' }}>{message}</p>
+        <p className="text-balck text-lg" style={{textDecoration: isChecked ? 'line-through' : '', fontStyle: isChecked ? 'italic': '', color: isChecked ? 'red': 'black' }}>{message}</p>
       </div>
       <div className="bg-gray-200 py-3 px-4 rounded-full cursor-pointer" onClick={deleteATodo}>
         <FontAwesomeIcon icon={faTrash} className="text-red-600" />
