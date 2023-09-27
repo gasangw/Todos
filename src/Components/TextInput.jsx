54372c68f2bb891bcf4fa71ea056ecbf3b5f50f0
gasangw <thomasgasangwa@gmail.com>
2023-09-27 16:34:27 +0200

    Add error message that is displayed when you try submitting an empty form

--- src/Components/TextInput.jsx
+++ src/Components/TextInput.jsx
@@ -4,6 +4,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import uuid from 'react-uuid';
 
 function TextInput({onAddTodo}) {
+  const [error, setError] = useState(false)
     const [todo, setTodo] = useState({
         id: uuid(),
         message: "",
@@ -17,20 +18,28 @@ function TextInput({onAddTodo}) {
             [name]: value
         }
        })
+       setError(false)
     }
 
     const addATodo =()=> {
-        if(todo.message === '')  return;
+        if(todo.message === ''){
+           setError(true)
+           return;
+        }
+        
         onAddTodo(todo)
         setTodo({id:uuid(), message: "", checked: false})
     } 
   return (
+    <>
+    {error && <p className='text-white bg-red-600 py-3 text-center font-semibold w-full absolute top-4'>The Input can't be empty, Kindly fill in something...</p>}
     <div className='flex justify-between rounded-full shadow-md w-3/5 mx-auto mt-8 px-6 items-center'>
     <input type='text' id={todo.id} onChange={inputTextHandler} placeholder='Add todo...' value={todo.message} name="message"  className='py-4 px-3 w-4/5 outline-none placeholder-black'/>
     <p className='bg-green-600 py-2 px-4 rounded-full cursor-pointer' onClick={addATodo}>
       <FontAwesomeIcon icon={faPlus}  className="text-[#f5f5f5]"/>
     </p>
    </div>
+  </>
   )
 }
 
